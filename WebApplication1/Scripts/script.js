0b2443bf2fdb5ce39af35152971c4558166c1471
Abdullah Farhan <nibir129@gmail.com>
2019-09-16 18:51:04 +0600

    first commit

---
+++ WebApplication1/Scripts/script.js
@@ -0,0 +1,14 @@
+﻿
+function clearVal() {
+	$('#firstNum').val("");
+	$('#secondNum').val("");
+	$('#result').val("");
+	
+}
+
+function sum() {
+	var firstNum = $('#firstNum').val();
+	var secondNum = $('#secondNum').val();
+	var result = parseInt(+firstNum) + parseInt(+secondNum);
+	$('#result').val(result);
+}
